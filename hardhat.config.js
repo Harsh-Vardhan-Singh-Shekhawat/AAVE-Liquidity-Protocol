4f88da4c95a4dc34c6693b8e119254391b3be8da
Harsh Shekhawat <harshshekhawat963@gmail.com>
2023-05-13 02:08:39 +0530

    liquidity pool protocol was built

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require('dotenv').config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks:{
+    goerli:{
+      url: process.env.GOERLI_ENDPOINT,
+      accounts:[process.env.PRIVATE_KEY]
+    }
+  }
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,31 +1,15 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const MarketInteraction = await hre.ethers.getContractFactory("MarketInteraction");
+  
+  const marketInteraction = await MarketInteraction.deploy('0x0496275d34753A48320CA58103d5220d394FF77F') //AAVE v3 testnet pooladdress provider
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  await marketInteraction.deployed();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log("Market Interaction Loan Contract has been deployed!", marketInteraction.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
